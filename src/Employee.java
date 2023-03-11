117eb54dd33ab18cc8feb0c708485ae97fa82b4a
Ldv236 <Ldv236@yandex.ru>
2023-03-12 01:36:01 +0200

    kr

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,51 @@
 public class Employee {
 
+    private static int countId;
+    private int id;
+    private String nameComponents;
+    private int department;
+    private int salary;
+
+    public int incrementId () {
+        return ++countId;
+    }
+
+    public Employee(String nameComponents, int department, int salary) {
+        this.id = incrementId();
+        this.nameComponents = nameComponents;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNameComponents() {
+        return nameComponents;
+    }
+
+    public void setNameComponents(String nameComponents) {
+        this.nameComponents = nameComponents;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
 public class Main {
+    static Employee[] employee = new Employee[10];
     public static void main(String[] args) {
         System.out.println("Курсовая работа 1");
     }
 
+    static String[] getInfo() {
+        return new String[10];
+    }
+
+    static int getSumSalary() {
+        return 1;
+    }
+
+    static Employee findMinSalary() {
+        return new Employee(" ", 1, 1);
+    }
+
+    static Employee findMaxSalary() {
+        return new Employee(" ", 1, 1);
+    }
+
+    static int calculateAverageSalary() {
+        return getSumSalary()/ employee.length;
+    }
+
+    static String[] getFioList() {
+        return new String[]{""};
+    }
 }
