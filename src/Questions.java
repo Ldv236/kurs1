0f73737d8be520dee571dda97ac26003ef584cf9
Ldv236 <Ldv236@yandex.ru>
2023-03-23 00:38:13 +0200

    bilet 2

---
+++ src/Questions.java
@@ -0,0 +1,62 @@
+public class Questions {
+
+    public static void main(String[] args) {
+
+        boolean bool = true;
+        int int1 = 0;
+        int int2 = 1;
+
+        if (bool) {
+            //true
+        } else if (int1 < int2) {
+            //true compare int
+        } else {
+            //other
+        }
+
+        bool = (int1 > int2 ? true : false);
+
+        switch (int1) {
+            case 0:
+                bool = true;
+                break;
+            case 1:
+                bool = false;
+                break;
+            default:
+                bool = !bool;
+        }
+        ;
+
+        bool = switch (int1) {
+            case 0 -> true;
+            case 1 -> false;
+            default -> !bool;
+        };
+
+        int[] intMas = new int[]{1, 2, 3, 4, 5};
+
+        for (int itmas : intMas) {
+            //do something
+        }
+
+        String str1 = "asdsa";
+        String str2 = "asddsa";
+        String str3 = "asdfg";
+
+        char[] charArray = str1.toCharArray();
+        System.out.println(checkPalindrome(charArray));
+        System.out.println(charArray.length / 2);
+    }
+
+    public static boolean checkPalindrome (char[] chars) {
+        for (int i = 0; i < chars.length / 2; i++) {
+            if (chars[i] != chars[chars.length - 1 - i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
